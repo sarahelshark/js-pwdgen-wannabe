ba5c4856dbd9ae69e67e380d936cae673a3a2c7f
sarahelshark <sarahelsh183@gmail.com>
2023-11-16 17:27:53 +0100

    prova 1 debug

--- myscript.js
+++ myscript.js
@@ -1,2 +1,11 @@
-
+/*prova 1
 alert('La mia sfera di cristallo dice che cerchi una password...');
+//dichiarazione variabile
+let Nome;
+//assegnazione
+Nome = 'Fabio';*///
+console.log('Hello world');
+
+
+
+console.log(document.getElementById('name'))
