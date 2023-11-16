4ff791c6293f69c36148711bea2383b4b9e1c68c
sarahelshark <sarahelsh183@gmail.com>
2023-11-16 18:04:20 +0100

    prova 4 variabili

--- myscript.js
+++ myscript.js
@@ -4,8 +4,22 @@ alert('La mia sfera di cristallo dice che cerchi una password...');
 let Nome;
 //assegnazione
 Nome = 'Fabio';*///
+
 console.log('Hello world');
+document.getElementById('Name');
+//debug
 
+//console.log(document.getElementById('name'));
+//selezione html tag
 
+const Name = 'Fabio'; //variabile username creata -DICHIARATA
+console.log(Name)//loggo in console la var creata-INIZIALIZZATA
 
-console.log(document.getElementById('name'))
+const Lastname = 'Pacifici'; // -DICHIARATA
+console.log(Lastname)//-INIZIALIZZATA
+
+const Color = 'Blu'; //-DICHIARATA
+console.log(Color)//-INIZIALIZZATA
+
+const password = Name + Lastname + Color
+console.log(password)
